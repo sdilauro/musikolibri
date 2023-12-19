df0d0519dd36692766020af0349b301e3def4114
Santiago <supersoniic@live.com>
2023-12-20 00:33:44 +0100

    Added dated paragraph component

--- src/app/uber-mich/page.tsx
+++ src/app/uber-mich/page.tsx
@@ -2,7 +2,8 @@
 
 import { ChakraProvider } from "@chakra-ui/react";
 import ResponsiveAppBar from "../../../components/navbar";
-import { Box, Text, ListItem, ListIcon, List, Image, Divider, defineStyle, defineStyleConfig } from '@chakra-ui/react'
+import DatedParagraph from "../../../components/dated_paragraph";
+import { Box, Text, ListItem, ListIcon, List, Image, Divider, defineStyle, defineStyleConfig, Flex, Center, Spacer } from '@chakra-ui/react'
 import { IoMdMusicalNote } from "react-icons/io";
 import { Gochi_Hand } from 'next/font/google'
 import { Oswald } from 'next/font/google'
@@ -60,7 +61,7 @@ export default function UberMichPage() {
                         >
                             Hallo! Ich bin Anabela
                         </Text>
-                        <List p={2} fontSize="1.2em">
+                        <List p={2} fontSize="1.25em">
                             <ListItem>
                                 <ListIcon as={IoMdMusicalNote} color='#E6175B' />
                                 Pianistin
@@ -109,122 +110,14 @@ export default function UberMichPage() {
                         align={'center'}
                     >
                         Mein Werdegang
-                        
+
                     </Text>
-                    
-                    <Box sx={{ paddingY: '2rem', display: 'flex', flexDir: 'row', width: '100%', alignItems: 'self-start' }} className='light'>
-                        <Box sx={{ mx: '3rem', display: 'flex', flexDir: 'column', flex: '-2', alignItems: 'self-start' }} className='light'>
-                            <Text
-                                noOfLines={1}
-                                bgGradient='linear(to-l, #7A59CA, #E6175B)'
-                                bgClip='text'
-                                className={gochi.className}
-                                fontSize='4xl'
-                                textAlign={'left'}
-                                sx={{ px: "0.5rem", marginTop: "1rem" }}
-                            >
-                                1990s
-                            </Text>
-                            
-                        </Box>
 
-                        <Box sx={{ mx: '3rem', display: 'flex', flexDir: 'column', flex: '5' }} className='light'>
-                            <Text
-                                fontSize='1.25rem'
-                                align={'left'}
-                                sx={{ fontcolor: "black", px: "0.5rem" }}
-                            >
-                                Schon als Kind hatte ich eine besondere Beziehung zur Musik. Ich erinnere mich zum Beispiel noch daran, wie ich als kleines Kind auf einem Spielzeugklavier gespielt habe, das eigentlich ein Geschenk für meinen älteren Bruder war. Ich komponierte bereits als junge Mädchen meine eigene Musik und hatte schon immer eine Neigung zum Musizieren und zum Erlernen von Musikinstrumenten.
-
-
-                            </Text>
-                        </Box>
-                    </Box>
-                    <Box sx={{ paddingY: '2rem', display: 'flex', flexDir: 'row', width: '100%', alignItems: 'self-start' }} className='light'>
-                        <Box sx={{ mx: '3rem', display: 'flex', flexDir: 'column', flex: '-2', alignItems: 'self-start' }} className='light'>
-                            <Text
-                                noOfLines={1}
-                                bgGradient='linear(to-l, #7A59CA, #E6175B)'
-                                bgClip='text'
-                                className={gochi.className}
-                                fontSize='4xl'
-                                textAlign={'left'}
-                                sx={{ px: "0.5rem", marginTop: "1rem" }}
-                            >
-                                2000s
-                            </Text>
-                            <Divider orientation="vertical" />
-                        </Box>
-                        <Box sx={{ mx: '3rem', display: 'flex', flexDir: 'column', flex: '5' }} className='light'>
-                            <Text
-                                fontSize='1.25rem'
-                                align={'left'}
-                                sx={{ fontcolor: "black", px: "0.5rem" }}
-                            >
-                                Aus familiären Gründen konnte ich erst mit 17 Jahren ein formales Musikstudium beginnen. Für einen relativ späten Start (viele professionelle Musiker beginnen ihr Studium im Kindesalter (!)) gelang es mir jedoch, meine Karriere sehr schnell voranzutreiben.
-                            </Text>
-                        </Box>
-                    </Box>
-                    <Box sx={{ paddingY: '2rem', display: 'flex', flexDir: 'row', width: '100%', alignItems: 'self-start' }} className='light'>
-                        <Box sx={{ mx: '3rem', display: 'flex', flexDir: 'column', flex: '-2', alignItems: 'self-start' }} className='light'>
-                            <Text
-                                noOfLines={1}
-                                bgGradient='linear(to-l, #7A59CA, #E6175B)'
-                                bgClip='text'
-                                className={gochi.className}
-                                fontSize='4xl'
-                                textAlign={'left'}
-                                sx={{ px: "0.5rem", marginTop: "1rem" }}
-                            >
-                                2010s
-                            </Text>
-                            <Divider orientation="vertical" />
-                        </Box>
-                        <Box sx={{ mx: '3rem', display: 'flex', flexDir: 'column', flex: '5' }} className='light'>
-                            <Text
-                                fontSize='1.25rem'
-                                align={'left'}
-                                sx={{ fontcolor: "black", px: "0.5rem" }}
-                            >
-                                In meiner Heimatstadt Buenos Aires studierte ich von 2011 bis 2017 Musik an der Universidad Nacional de las Artes mit dem Schwerpunkt Klavier. Während dieser Zeit habe ich auch viele Konzerte in verschiedenen Konzertsälen im ganzen Land gegeben.
-
-                                Am Ende meines Studiums habe ich mich weitergebildet. Besonders hervorzuheben ist ein einjähriger Aufenthalt in Barcelona, um bei Maestro Josep Culom zu studieren.
-                            </Text>
-                        </Box>
-                    </Box>
-                    <Box sx={{ paddingY: '2rem', display: 'flex', flexDir: 'row', width: '100%', alignItems: 'self-start' }} className='light'>
-                        <Box sx={{ mx: '3rem', display: 'flex', flexDir: 'column', flex: '-2', alignItems: 'self-start' }} className='light'>
-                            <Text
-                                noOfLines={1}
-                                bgGradient='linear(to-l, #7A59CA, #E6175B)'
-                                bgClip='text'
-                                className={gochi.className}
-                                fontSize='4xl'
-                                textAlign={'left'}
-                                sx={{ px: "0.5rem", marginTop: "1rem" }}
-                            >
-                                2020s
-                            </Text>
-                            <Divider orientation="vertical" />
-                        </Box>
-                        <Box sx={{ mx: '3rem', display: 'flex', flexDir: 'column', flex: '5' }} className='light'>
-                            <Text
-                                fontSize='1.25rem'
-                                align={'left'}
-                                sx={{ fontcolor: "black", px: "0.5rem" }}
-                            >
-                                Im Jahr 2022 wurde meine Tochter geboren und ich entdeckte die wunderbare Welt der Mutterschaft. Zu dieser Zeit wurde mir klar, dass ich Musikunterricht für Babys und Kleinkinder wirklich beruflich machen wollte.
-                            </Text>
-                        </Box>
-                    </Box>
-                    <Text
-                                fontSize='1.25rem'
-                                align={'left'}
-                                sx={{ fontcolor: "black", px: "0.5rem" }}
-                            >
-                                Was mich aber mein ganzes Leben lang am meisten geprägt hat, ist das Unterrichten. Seit meinem 19. Lebensjahr unterrichte ich Klavier und genieße es jeden Tag ein bisschen mehr. Ich habe Schüler aller Altersstufen unterrichtet und war immer fasziniert von den Gemeinsamkeiten und Unterschieden zwischen meinen jüngeren und älteren Schülern.
-                            </Text>
-                </Box>
+                    <DatedParagraph date="1990s" paragraph="Schon als Kind hatte ich eine besondere Beziehung zur Musik. Ich erinnere mich zum Beispiel noch daran, wie ich als kleines Kind auf einem Spielzeugklavier gespielt habe, das eigentlich ein Geschenk für meinen älteren Bruder war. Ich komponierte bereits als junge Mädchen meine eigene Musik und hatte schon immer eine Neigung zum Musizieren und zum Erlernen von Musikinstrumenten." id={0}/>
+                    <DatedParagraph date="2000s" paragraph="Aus familiären Gründen konnte ich erst mit 17 Jahren ein formales Musikstudium beginnen. Für einen relativ späten Start (viele professionelle Musiker beginnen ihr Studium im Kindesalter (!)) gelang es mir jedoch, meine Karriere sehr schnell voranzutreiben." id={1}/>
+                    <DatedParagraph date="2010s" paragraph="In meiner Heimatstadt Buenos Aires studierte ich von 2011 bis 2017 Musik an der Universidad Nacional de las Artes mit dem Schwerpunkt Klavier. Während dieser Zeit habe ich auch viele Konzerte in verschiedenen Konzertsälen im ganzen Land gegeben. Am Ende meines Studiums habe ich mich weitergebildet. Besonders hervorzuheben ist ein einjähriger Aufenthalt in Barcelona, um bei Maestro Josep Culom zu studieren." id={2}/>
+                    <DatedParagraph date="2020s" paragraph="Im Jahr 2022 wurde meine Tochter geboren und ich entdeckte die wunderbare Welt der Mutterschaft. Zu dieser Zeit wurde mir klar, dass ich Musikunterricht für Babys und Kleinkinder wirklich beruflich machen wollte." id={3}/>
+                 </Box>
             </Box>
         </ChakraProvider>
     )
