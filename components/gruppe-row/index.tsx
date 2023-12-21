99b26c6594d810f3fa87da4827f59f33e8d04aaa
Santiago <supersoniic@live.com>
2023-12-22 00:07:11 +0100

    Add form page and link to it

--- components/gruppe-row/index.tsx
+++ components/gruppe-row/index.tsx
@@ -1,58 +1,59 @@
 'use client'
 
-import { Box, Button, Card, CardBody, Flex, Text } from '@chakra-ui/react';
+import { Box, Button, Card, CardBody, Flex, Text, Link } from '@chakra-ui/react';
 import { Gochi_Hand, Oswald } from 'next/font/google';
 import './../../styles/App.css';
- 
+
 const oswald = Oswald({
     weight: '400',
     subsets: ['latin'],
-  })
-  
-  const gochi = Gochi_Hand({
+})
+
+const gochi = Gochi_Hand({
     weight: '400',
     subsets: ['latin'],
-  })
-   
-type GruppeCardProps = { name: string, schedule: string, days:string, color:string, id:number }
+})
+
+type GruppeCardProps = { name: string, schedule: string, days: string, color: string, id: number }
 
 
-const GruppeRow = (props:GruppeCardProps) => {
-    
+const GruppeRow = (props: GruppeCardProps) => {
+
     return (
-        <Card 
+        <Card
             direction={{ base: 'column', sm: 'row' }}
             overflow='hidden'
             variant='outline'
-            backgroundColor={'#f4e4dc'} 
-            borderRadius={'32px'} 
-            padding='2rem' 
+            backgroundColor={'#f4e4dc'}
+            borderRadius={'32px'}
+            padding='2rem'
             margin='2rem'
             maxW='%60'
             key={props.id}>
             <CardBody>
                 <Flex flexDir='column' flex='1' alignItems='top'>
-                    <Text 
-                    marginBottom={'-1rem'}
-                    fontSize={'48px'} 
-                    className={gochi.className} 
-                    bgGradient='linear(to-l, #7A59CA, #E6175B)'
-                    bgClip='text'> 
-                        {props.name} 
+                    <Text
+                        marginBottom={'-1rem'}
+                        fontSize={'48px'}
+                        className={gochi.className}
+                        bgGradient='linear(to-l, #7A59CA, #E6175B)'
+                        bgClip='text'>
+                        {props.name}
                     </Text>
-                    <Text 
-                    fontSize={'28px'} 
-                    className={oswald.className}>
+                    <Text
+                        fontSize={'28px'}
+                        className={oswald.className}>
                         Termin
                     </Text>
                     <Box display={'flex'} flexDir={'row'} alignItems={'center'} marginY={'1rem'} justifyContent={'space-between'} width={'100%'}>
                         <Text className={oswald.className} fontSize={'24px'}>Donnerstag</Text>
                         <Text className={oswald.className} fontSize={'24px'}>{props.schedule}</Text>
                         <Text className={oswald.className} fontSize={'24px'}>{props.days}</Text>
-                        <Button paddingX='5rem' colorScheme={'purple'}variant='solid' borderRadius={'1.5rem'} h={'3rem'} >
-                            
-                            <Text className={gochi.className} fontSize={'24px'}>Auswählen</Text>
-                        </Button>
+                        <Link style={{ textDecoration: 'none' }} href={'/formular'}>
+                            <Button paddingX='5rem' colorScheme={'purple'} variant='solid' borderRadius={'1.5rem'} h={'3rem'} >
+                                <Text className={gochi.className} fontSize={'24px'}>Auswählen</Text>
+                            </Button>
+                        </Link>
                     </Box>
                 </Flex>
             </CardBody>
