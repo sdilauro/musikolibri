d9d98e3714d175dae39e9f08b349bef7f57fb5ef
Santiago <supersoniic@live.com>
2024-01-17 22:55:53 +0100

    finished thank you page

--- pages/danke.tsx
+++ pages/danke.tsx
@@ -25,7 +25,7 @@ export default function DankePage() {
     const [isSmallScreen] = useMediaQuery(maxWidth);
     return (
         <Box sx={{ w: '100%' }} className='light'>
-        <Box sx={{ paddingY: '2rem', paddingBottom: '6rem', display: 'flex', flexDir: 'column', maxWidth: '1440px' }} width={isSmallScreen?'80%':'70%'} className='light'>
+        <Box sx={{ w: '40%', paddingY: '2rem', paddingBottom: '6rem', display: 'flex', flexDir: 'column', maxWidth: '1440px' }} width={isSmallScreen?'80%':'70%'} className='light'>
             <Text
                 noOfLines={2}
                 bgGradient='linear(to-l, #7A59CA, #E6175B)'
@@ -33,17 +33,30 @@ export default function DankePage() {
                 className={gochi.className}
                 fontSize={isSmallScreen?'4xl':'5xl'}
                 align={'center'}
+                sx={{ paddingBottom: '2rem'}}
             >
-                Danke für deine Anmeldung!
+                Danke für eure Anmeldung!
             </Text>
             <Text
                 fontSize='1.25rem'
                 align={'left'}
-                sx={{ fontcolor: "black" }}
+                sx={{ fontcolor: "black",  paddingBottom: '2rem' }}
                 textAlign='justify'
             >
-                {addLineBreak("Wir haben die Anmeldung für den Musikworkshop deiner kleinen Maus erhalten. \n\n Wir werden uns in Kürze mit dir in Verbindung setzen, um dir weitere Informationen zum Workshop zukommen zu lassen.")}
+                {addLineBreak("Ich habe eure Anmeldung für den Musikworkshop eurer kleinen Maus erhalten. \n\n Eine Bestätigungsmail wurde soeben an die angegebene Adresse geschickt. (Bitte überprüft ggf. euren Spam-Ordner). \n\n Ich werde mich in Kürze mit weiteren Informationen zum Workshop bei euch melden.")}
             </Text>
+            <Box sx={{display: 'flex', flex: '0.5' }} flexDir={'row'} alignItems={'center'} w={'100%'} className='light'>
+                        <Image
+                            width='25rem'
+                            src='/assets/registration-success.png'
+                            alt='Anabela'
+                            fallbackSrc='https://via.placeholder.com/512'
+                            borderRadius={'4'}
+                            position={'relative'}
+                            objectFit='contain'
+                            mt={isSmallScreen?'2rem':'0rem'}
+                        />
+                    </Box>
         </Box>
     </Box>
 
