714b26b22c36e018ade797aedcd023e839dd561b
SebaDL <jsdilauro@gmail.com>
2024-02-15 02:55:20 -0300

    delete wrong files (sorry about that) - add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,15 @@ import { AppProps } from 'next/app'
 import './../styles/App.css'
 import ResponsiveAppBar from '../components/navbar'
 import Footer from '../components/footer'
+import Head from 'next/head'
 
 
 function MyApp({Component, pageProps}:AppProps) {
   return(
     <ChakraProvider>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Flex minH={'100vh'} flexDir={'column'}>
         <ResponsiveAppBar />
           <Box flex={1} alignContent={'center'}>
